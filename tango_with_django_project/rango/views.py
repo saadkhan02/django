974df686a0f5bccc9fac27a22606689dd1f6dc66
Saad Khan <saad.khan02@gmail.com>
2016-07-29 02:25:36 -0400

    Support new about page with stuff from templates and static files. Also change home page to be template based.

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -2,4 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says, Hey there partner!")
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    return render(request, 'rango/index.html', context=context_dict)
+
+def about(request):
+    content = {'aboutmessage': 'Coz Rango says so!'}
+    return render(request, 'rango/about.html', context=content)
